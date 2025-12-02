78f10ad2eb721fa46cd1f0df93b2c915e57cc66d
sxpydo <shavypeynado@gmail.com>
2025-12-02 20:21:38 +0000

    Testing: Setted up Vitest and created setupTests.ts file and installed relevant packages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config' 
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+    test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: ['node_modules/', 'dist/', 'src/main.tsx', 'src/vite-env.d.ts'],
+    },
+  },
+});
